e4f80d26597b96fb2a929b12dafe38d743056fea
GoldenJayz <jmdaniels150@gmail.com>
2021-06-26 09:14:55 -0400

    aaa

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="192.168.1.14", port="4200") 
